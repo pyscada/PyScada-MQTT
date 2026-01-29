b62555db6b4488344e6f5e0ed9466257a7849eb4
Martin Schroeder <m.schroeder@tu-berlin.de>
2026-01-29 17:43:30 +0000

    removed unused import from view

--- pyscada/mqtt/views.py
+++ pyscada/mqtt/views.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import traceback
-
-from django.conf import settings
-from django.shortcuts import render
-from django.http import Http404
-from django.views.decorators.csrf import requires_csrf_token
-from django.template.loader import get_template
-from django.template.response import TemplateResponse
-
-from pyscada.core import version as core_version
-
 import logging
 
 logger = logging.getLogger(__name__)
